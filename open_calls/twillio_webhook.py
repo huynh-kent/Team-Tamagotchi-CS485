b5b4eb3cf2acbe546dbea6b777e22c44ac405532
huynh-kent <huynhkentd@gmail.com>
2023-02-15 12:29:21 -0800

    added pickling, user folder

--- open_calls/twillio_webhook.py
+++ open_calls/twillio_webhook.py
@@ -6,32 +6,44 @@ from tools.logging import logger
 
 import random
 import json
+import pickle
 
 yml_configs = {}
 BODY_MSGS = []
 with open('config.yml', 'r') as yml_file:
     yml_configs = yaml.safe_load(yml_file)
 
-import random
-import json
-
+# corpus 
 CORPUS = {}
-
 with open('chatbot_corpus.json', 'r') as myfile:
     CORPUS = json.loads(myfile.read())
 
 def handle_request():
     logger.debug(request.form)
 
-    response = 'NOT FOUND'
+    # pickling
+    act = None
+    if exists(f"users/{request.form['From']}.pkl"):
+        with open(f"users/{request.form['From']}.pkl", 'rb') as p:
+            actor = pickle.load(p)
+    else:
+        act = actor(request.form['From'])
 
+    act.save_msg(request.form['Body'])
+    logger.debug(act.prev_msgs)
+    with open(f"user/{request.form['Form']}.pkl", 'wb') as p:
+        pickle.dump(act,p)
+
+    response = 'response here'
+
+    # checking if input in corpus
     sent_input = str(request.form['Body']).lower()
     if sent_input in CORPUS['input']:
         response = random.choice(CORPUS['input'][sent_input])
     else:
         CORPUS['input'][sent_input] = ['DID NOT FIND']
         with open('chatbot_corpus.json', 'w') as myfile:
-            myfile.write(json.dumps(CORPUS, indent=4 ))
+            myfile.write(json.dumps(CORPUS, indent=4))
 
     logger.debug(response)
 
@@ -41,3 +53,6 @@ def handle_request():
                      to=request.form['From'])
     
     return json_response( status = "ok" )
+
+# tamagotchi outline
+tamagotchi_outline = "                     🌸🌸🌸\n             🌸🌸🌸🌸🌸🌸\n        🌸🌸🌸🌸🌸🌸🌸🌸\n     🌸🌸🌸🌸🌸🌸🌸🌸🌸\n  🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸\n🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸\n🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸\n🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸\n🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸\n🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸\n🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸\n🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸\n🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸\n🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸\n   🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸\n     🌸🌸🌸🌸🌸🌸🌸🌸🌸\n        🌸🌸🌸🌸🌸🌸🌸🌸\n             🌸🌸🌸🌸🌸🌸\n                  🌸🌸🌸🌸"
