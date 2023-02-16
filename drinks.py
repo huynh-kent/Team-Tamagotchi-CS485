5e27be3656a7ca034b1b0a616f8336045fa99f59
huynh-kent <huynhkentd@gmail.com>
2023-02-15 17:04:15 -0800

    added drinks

---
+++ drinks.py
@@ -0,0 +1,39 @@
+import random
+
+all_drinks = ['🥛','🍼','🫖','☕️','🍵','🧃','🥤','🧋','🍶',
+              '🍺','🍻','🥂','🍷','🥃','🍸','🍹','🧉','🍾',
+              ]
+
+class drinks:
+    choices = {}
+    drink_thirst = []
+
+    def __init__(self):
+        self.choices = random.choices(all_drinks, k=3)
+        for drink in self.choices:
+            self.drink_thirst.append(random.randint(1, 10))
+
+    def show_choices(self):
+        for count,choice in enumerate(self.choices):
+            print(f"{choice} --- {self.drink_thirst[count]}")
+
+    def give_drink_choices(self):
+        drink_options = []
+        for count,choice in enumerate(self.choices):
+            drink_option = drink(choice, self.drink_thirst[count])
+            drink_options.append(drink_option)
+            print(drink_option.emoji, drink_option.thirst, end=' ')
+        return drink_options
+        
+
+        
+class drink:
+    emoji = ''
+    thirst = 0
+
+    def __init__(self, emoji, thirst):
+        self.emoji = emoji
+        self.thirst = thirst
+
+drink_test = drinks()
+drink_test.give_drink_choices()
