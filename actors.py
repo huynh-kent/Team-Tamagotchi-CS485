e9791d407dbffd6e9e635bcdf8a9022130dbe8de
mikenziebrown <105826033+mikenziebrown@users.noreply.github.com>
2023-02-19 20:34:11 -0800

    added state variable

--- actors.py
+++ actors.py
@@ -1,7 +1,14 @@
 class actor:
+
     def __init__(self, phone_number):
         self.phone = phone_number
         self.prev_msgs = []
+        self.state = "begin"
+        print('this is created:')
+        print(self.state)
 
     def save_msg(self, msg):
         self.prev_msgs.append(msg)
+
+    def change_state(self, new_state):
+        state = new_state
