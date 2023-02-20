2f9c5517ce99717cbcac2258506fcf3d59f5888f
mikenziebrown <105826033+mikenziebrown@users.noreply.github.com>
2023-02-19 20:33:23 -0800

    calls game_script.json and reads actor state

--- open_calls/twillio_webhook.py
+++ open_calls/twillio_webhook.py
@@ -16,7 +16,7 @@ with open('config.yml', 'r') as yml_file:
 
 CORPUS = {}
 
-with open('chatbot_corpus.json', 'r') as myfile:
+with open('game_script.json', 'r') as myfile:
     CORPUS = json.loads(myfile.read())
 
 def handle_request():
@@ -26,34 +26,50 @@ def handle_request():
 
     # pickling
     act = None
+    first_time = False
     if os.path.exists(f"users/{request.form['From']}.pkl"):
         with open(f"users/{request.form['From']}.pkl", 'rb') as p:
             act = pickle.load(p)
+            response = 'already used number'
+            logger.debug('current state:')
+            logger.debug(act.state)
     else:
         act = actor(request.form['From'])
+        response = (CORPUS['init']['content'])
+        logger.debug('current state:')
+        logger.debug(act.state)
+        first_time = True
     act.save_msg(request.form['Body'])
     logger.debug(act.prev_msgs)
     with open(f"users/{request.form['From']}.pkl", 'wb') as p:
         pickle.dump(act,p)
 
     # corpus
-    sent_input = str(request.form['Body']).lower()
-    if sent_input in CORPUS['input']:
-        response = random.choice(CORPUS['input'][sent_input])
-    else:
-        CORPUS['input'][sent_input] = ['DID NOT FIND']
-        with open('chatbot_corpus.json', 'w') as myfile:
-            myfile.write(json.dumps(CORPUS, indent=4 ))
+    ####if first_time = false:    #on our first time, we have already deter
+      #### state =
+       #### response = (CORPUS[state]
 
-    # response back
-    response = 'response here'
+        """
+        sent_input = str(request.form['Body']).lower()
+        if sent_input in CORPUS['input']:
+            response = random.choice(CORPUS['input'][sent_input])
+        else:
+            CORPUS['input'][sent_input] = ['DID NOT FIND']
+            with open('chatbot_corpus.json', 'w') as myfile:
+                myfile.write(json.dumps(CORPUS, indent=4 ))
+        """
+
+   #  response back
+  ## response = 'response here'
+
     logger.debug(response)
     message = g.sms_client.messages.create(
                      body=response,
                      from_=yml_configs['twillio']['phone_number'],
                      to=request.form['From'])
-    
+
     return json_response( status = "ok" )
+
 
 # tamagotchi outline
 tamagotchi_outline = """                     🌸🌸🌸
@@ -75,3 +91,5 @@ tamagotchi_outline = """                     🌸🌸🌸
                                                                            🌸🌸🌸🌸🌸🌸🌸🌸
                                                                                                      🌸🌸🌸🌸🌸🌸
                                                                                                                                          🌸🌸🌸🌸"""
+
+                                                                                                                                                                                                      94,0-1        Bot
