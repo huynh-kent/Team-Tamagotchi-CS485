5efb5c57b68e1a8c598b8d0be154f6b5a1fc752f
huynh-kent <huynhkentd@gmail.com>
2023-02-21 15:05:14 -0800

    moved pickling here

--- pickles.py
+++ pickles.py
@@ -2,16 +2,30 @@ import pickle
 from actors import actor
 from tools.logging import logger
 import os
+import json
+
+CORPUS = {}
+with open('game_script.json', 'r') as myfile:
+    CORPUS = json.loads(myfile.read())
 
 def pickling(form):
     # pickling
     act = None
+    first_time = True
     if os.path.exists(f"users/{form['From']}.pkl"):
         with open(f"users/{form['From']}.pkl", 'rb') as p:
             act = pickle.load(p)
+            response = (CORPUS[act.state]['content'])
     else:
         act = actor(form['From'])
+        response = (CORPUS['begin']['content'])
+        first_time = False
+
     act.save_msg(form['Body'])
     logger.debug(act.prev_msgs)
+    act.state = (CORPUS[act.state]['next_state'])
     with open(f"users/{form['From']}.pkl", 'wb') as p:
         pickle.dump(act,p)
+    logger.debug('current state:')
+    logger.debug(act.state)
+    return response
