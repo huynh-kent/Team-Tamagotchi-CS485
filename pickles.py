c17ce8c5350bdbd7180cdc9cba183c79df23d7fb
huynh-kent <huynhkentd@gmail.com>
2023-02-21 15:27:14 -0800

    cleaned up code

--- pickles.py
+++ pickles.py
@@ -11,21 +11,25 @@ with open('game_script.json', 'r') as myfile:
 def pickling(form):
     # pickling
     act = None
-    first_time = True
-    if os.path.exists(f"users/{form['From']}.pkl"):
+    # returning user
+    if os.path.exists(f"users/{form['From']}.pkl"):   
         with open(f"users/{form['From']}.pkl", 'rb') as p:
             act = pickle.load(p)
             response = (CORPUS[act.state]['content'])
+    # new user
     else:
         act = actor(form['From'])
         response = (CORPUS['begin']['content'])
-        first_time = False
 
+    # save into message history
     act.save_msg(form['Body'])
     logger.debug(act.prev_msgs)
+
+    # set new state
     act.state = (CORPUS[act.state]['next_state'])
+    logger.debug(f'current state: {act.state}')
+
     with open(f"users/{form['From']}.pkl", 'wb') as p:
         pickle.dump(act,p)
-    logger.debug('current state:')
-    logger.debug(act.state)
+
     return response
