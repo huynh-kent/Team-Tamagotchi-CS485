9b3575cdb954b2938aaaf5413b6f6b6962707553
huynh-kent <huynhkentd@gmail.com>
2023-02-22 10:42:14 -0800

    removed comments

--- open_calls/twillio_webhook.py
+++ open_calls/twillio_webhook.py
@@ -5,18 +5,6 @@ from pickles import pickling, save_pickle, pickle
 from send_message_back import send_message, send_picture
 from processing_message import process_message
 
-"""
-from apscheduler.schedulers.background import BackgroundScheduler
-scheduler = BackgroundScheduler()
-def time_passed(user):
-    user.tamagotchi.time_tick()
-    return user
-
-def delayed_pickle(user):
-    with open(f"users/{user.phone}.pkl", 'wb') as p:
-        pickle.dump(user,p)
-"""
-
 ### Main
 def handle_request():
     # user info
@@ -33,25 +21,10 @@ def handle_request():
 
     # send message back/game state from send_message_back.py
     send_message(user.phone, response)
+    
     # send picture name/url from media.yml
     #picture_name = 'sample-tamagotchi-image'
     #send_picture(request.form, picture_name)
-
-    # test
-    #send_message(request.form, get_tamagotchi())
-
-    # delayed pickle
-    #scheduler.add_job(lambda: delayed_pickle(user), trigger="interval", seconds=360, max_instances=1)
-
-    """
-    # time_tick
-    if user.tamagotchi is not None:
-        scheduler.add_job(lambda: time_passed(user), trigger="interval", seconds=10)
-    try:
-        scheduler.start()
-    except Exception:
-        print('scheduler running')
-    """
 
     # save pickle/user
     save_pickle(user)
