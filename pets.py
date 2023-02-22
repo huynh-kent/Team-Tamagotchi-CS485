14a725028f92539af576b45ffef0287f0476a17f
huynh-kent <huynhkentd@gmail.com>
2023-02-22 00:17:21 -0800

    added for choosing pet

---
+++ pets.py
@@ -0,0 +1,50 @@
+import random
+
+all_pets = ['🍏', '🍎','🍐','🍊','🍋','🍌','🍉','🍇','🍓','🫐','🍈',
+             '🍒','🍑','🥭','🍍','🥥','🥝','🍅','🍆','🥑','🥦','🥬',
+             '🥒','🌶️','🫑','🌽','🥕', '🫒','🧄','🧅','🥔','🍠','🥐',
+             '🥯','🍞','🥖','🥨','🧀','🥚','🍳','🧈','🥞','🧇','🥓',
+             '🥩','🍔','🌭','🦴','🍖','🍗','🍟','🍕','🫓','🥪','🥙',
+             '🥗','🫔','🌯','🌮','🧆','🥘','🫕','🥫','🫙','🍝','🍱',
+             '🍣','🍛','🍲','🍜','🥟','🦪','🍤','🍙','🍚','🍘','🍥',
+             '🥠','🥮','🍢','🍡','🍧','🍨','🍦','🥧','🍭','🍮','🎂',
+             '🍰','🧁','🍬','🍫','🍿','🍩','🍪','🍯','🫘','🥜','🌰']
+
+
+
+class pets:
+    choices = {}
+    #pet_hunger = []
+
+    def __init__(self):
+        self.choices = random.choices(all_pets, k=3)
+        #for pet in self.choices:
+        #    self.pet_hunger.append(random.randint(1, 10))
+
+    def show_choices(self):
+        #for count,choice in enumerate(self.choices):
+        #    print(f"{choice} --- {self.pet_hunger[count]}")
+
+        for choice in self.choices:
+            print(choice, end=' ')
+
+    def give_pet_choices(self):
+        """pet_options = []
+        for count,choice in enumerate(self.choices):
+            pet_option = pet(choice, self.pet_hunger[count])
+            print(pet_option.emoji, pet_option.hunger, end=' ')
+            pet_options.append(pet_option)
+        return pet_options"""
+        
+        
+class pet:
+    emoji = ''
+    hunger = 0
+
+    def __init__(self, emoji):
+        self.emoji = emoji
+
+
+test = pets()
+test.give_pet_choices()
+
