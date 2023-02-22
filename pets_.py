7601f8ec9bffeab112824c8c1871c518c5dedc71
huynh-kent <huynhkentd@gmail.com>
2023-02-22 12:10:17 -0800

    removed set

--- pets_.py
+++ pets_.py
@@ -13,7 +13,7 @@ class pets:
     pet_options = []
     choices = {}
     def __init__(self):
-        self.choices = set(random.choices(all_pets, k=3))
+        self.choices = (random.choices(all_pets, k=3))
         #for pet in self.choices:
         #    self.pet_hunger.append(random.randint(1, 10))
 
