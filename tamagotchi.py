9ae59c706f1c6bb9057fa89cf11a48204344c333
huynh-kent <huynhkentd@gmail.com>
2023-02-22 12:09:44 -0800

    in need boolean

--- tamagotchi.py
+++ tamagotchi.py
@@ -35,6 +35,8 @@ class Tamagotchi:
         if self.hunger < 50 or self.bored > 50 or self.thirst < 50:
             self.happiness -= 5
             self.in_need = True
+        else:
+            self.in_need = False
 
         if self.hunger <= 0 or self.bored >= 100 or self.thirst <= 0 or self.happiness <= 0:
             self.alive = False
