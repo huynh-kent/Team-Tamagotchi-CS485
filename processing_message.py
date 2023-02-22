1a6172b172691f7d282f44cd69768c5dd2966089
huynh-kent <huynhkentd@gmail.com>
2023-02-22 03:36:50 -0800

    set up states

--- processing_message.py
+++ processing_message.py
@@ -12,17 +12,28 @@ with open('game_script.json', 'r') as myfile:
     CORPUS = json.loads(myfile.read())
 
 def process_message(user, sent_input):
-    if user.state == 'begin':
-        pet_choices = pets()
-        response = f'Hi, please pick a pet!\n{pet_choices.show_choices()}'
-        user.state = CORPUS[user.state]['next_state']
-    elif sent_input in CORPUS[user.state]['response']:
-        response = CORPUS[user.state]['response'][sent_input]
-        user.state = CORPUS[user.state]['next_state']
-    else:
-        response = "Please try again"
+    user.prev_state = user.state
 
+    if user.state == 'begin':
+        content = f"{CORPUS[user.state]['content']}"
+    elif user.state == 'choose':
+        pet_choices = pets()
+        content = f"{CORPUS[user.state]['content']}"
+    elif user.state == 'name':
+        pass
+    elif user.state == 'confirmation':
+        if user.prev_state == 'choose':
+            pass
+        elif user.prev_state == 'name':
+            pass
+    #elif sent_input in CORPUS[user.state]['response']:
+    #    response = CORPUS[user.state]['response'][sent_input]
+    #    user.state = CORPUS[user.state]['next_state']
+    else:
+        content = "Please try again"
+
+    user.state = CORPUS[user.state]['next_state']    
     # check state
     logger.debug(user.state)
 
-    return (user, response)
+    return (user, content)
