ad4649a9a11695ddcdce65c2e651b6dff04e85e6
huynh-kent <huynhkentd@gmail.com>
2023-02-21 16:12:07 -0800

    test

--- tamagotchi.py
+++ tamagotchi.py
@@ -48,9 +48,9 @@ class Tamagotchi:
 
 def get_outline():
     # tamagotchi outline
-    tamagotchi_outline = """
+    tamagotchi_outline = """Hello, this is how your Tamagotchi is looking!
 \n
-              \t123🌸🌸🌸🌸🌸🌸
+                        🌸🌸🌸🌸🌸🌸
          🌸🌸🌸🌸🌸🌸🌸🌸
       🌸🌸🌸🌸🌸🌸🌸🌸🌸
    🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸
