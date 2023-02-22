70206fbfdfa10f2b1d9e815de16195c4e58d4b3a
huynh-kent <huynhkentd@gmail.com>
2023-02-22 04:32:04 -0800

    format

--- processing_message.py
+++ processing_message.py
@@ -22,7 +22,8 @@ def process_message(user, sent_input):
         content = f"{content}"
     elif user.state == 'choose':
         pet_choices = pets()
-        content = f"{(CORPUS[user.state]['content'])+{pet_choices.show_choices()}}"
+        content = (CORPUS[user.state]['content'])
+        content = f"{content+(pet_choices.show_choices())}"
     elif user.state == 'name':
         pass
     elif user.state == 'confirmation':
