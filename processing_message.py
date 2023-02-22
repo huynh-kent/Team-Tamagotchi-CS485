1b7c005e99ce70b1127a8f1cfafff6d467581da6
huynh-kent <huynhkentd@gmail.com>
2023-02-22 03:59:49 -0800

    json dumps

--- processing_message.py
+++ processing_message.py
@@ -22,7 +22,7 @@ def process_message(user, sent_input):
         content = f"{content}"
     elif user.state == 'choose':
         pet_choices = pets()
-        content = json.loads(CORPUS[user.state]['content'])
+        content = json.dumps(CORPUS[user.state]['content'])
         content = f"{content}"
     elif user.state == 'name':
         pass
