7ef10406ed368dbb92ebfb820f0a9d49e5a95564
huynh-kent <huynhkentd@gmail.com>
2023-02-22 13:58:44 -0800

    clean

--- pets_.py
+++ pets_.py
@@ -16,8 +16,6 @@ class pets:
         self.choices = random.sample(all_pets, 3)
 
     def show_choices(self):
-        #for count,choice in enumerate(self.choices):
-        #    print(f"{choice} --- {self.pet_hunger[count]}")
         pet_string = ''
         for choice in self.choices:
             self.pet_options.append(choice)
