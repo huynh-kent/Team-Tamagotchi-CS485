421a9df5d159e517e573ea8b49fe0daa5db5d10f
huynh-kent <huynhkentd@gmail.com>
2023-02-22 23:21:17 -0800

    remove print statement

--- actors.py
+++ actors.py
@@ -7,15 +7,13 @@ class actor:
         self.prev_msgs = []
         self.state = "begin"
         self.prev_state = ''
-#        print('this is created:')
-#        print(self.state)
         self.tamagotchi = None
 
     def save_msg(self, msg):
         self.prev_msgs.append(msg)
 
     def change_state(self, new_state):
-        state = new_state
+        self.state = new_state
 
     def create_tamagotchi(self, pet):
         self.tamagotchi = Tamagotchi(pet)
