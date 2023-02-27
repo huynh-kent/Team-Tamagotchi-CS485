bcedca896d62116337b8b5cd89d0f3bbc5296ebc
jesusmentado <124843869+jesusmentado@users.noreply.github.com>
2023-02-26 19:08:01 -0800

    Update processing_message.py
    edit changes

--- processing_message.py
+++ processing_message.py
@@ -112,7 +112,6 @@ def process_message(user, sent_input):
                 while t:
                     mins, secs = divmod(t,60)
                     timer = '{:02d}:{:02d}'.format(mins, secs)
-                    send_message(user.phone, f"{timer}")
                     time.sleep(1)
                     t -= 1
 
